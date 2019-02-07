85b17ab5ec8956ce642b1e5f2f3f13d51671202f
your name here <theo@dali.dartmouth.edu>
2019-02-07 14:36:11 -0800

    add code

---
+++ throttle.js
@@ -0,0 +1,27 @@
+function throttle(func, timeLimit) {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if ((Date.now() - lastRan) >= timeLimit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, timeLimit - (Date.now() - lastRan));
+    }
+  }
+}
+
+let colorChange = throttle(function () {
+  let colors = ['orange', 'red', 'blue', 'green', 'yellow'];
+  let btn = document.getElementsByClassName('throttle')
+  let num = Math.floor((10 * (Math.random(0))) % colors.length)
+  btn[0].style.backgroundColor = `${colors[num]}`
+}, 2000)
